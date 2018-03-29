17d8b71b6e98673fa9a8b2483d6d64384e6dae4a
shallin <sucuilin27@sina.com>
2018-03-29 23:48:01 +0800

    Initial commit

---
+++ src/main/java/com/blogger/blogger/domain/User.java
@@ -0,0 +1,45 @@
+package com.blogger.blogger.domain;
+
+  import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 用户实体
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
